3ffb087d5def0a5ecd9466db4bf552163ca6601a
unknown <miha.vasilyev@mail.ru>
2022-07-01 15:58:14 +0300

    удалены пробелы

--- convert.py
+++ convert.py
@@ -4,11 +4,11 @@ frame=6
 video= cv2.VideoCapture("c.mp4")
 success, image = video.read()
 while success:
-                success,image = video.read()
-                if frame==6:
-                                resize = cv2.resize(image, (1280, 960)) 
-                                cv2.imwrite("%03d.jpg" % count, resize)
-                                count+=1
-                                frame=0
-                else:
-                                frame+=1
+    success,image = video.read()
+    if frame==6:
+        resize = cv2.resize(image, (1280, 960)) 
+        cv2.imwrite("%03d.jpg" % count, resize)
+        count+=1
+        frame=0
+    else:
+        frame+=1
