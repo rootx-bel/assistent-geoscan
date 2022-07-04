f07fa2f1c69e2c2fec41d8c9d727cab150d8c7cf
RussianBear391 <95121773+RussianBear391@users.noreply.github.com>
2022-07-04 14:59:37 +0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,15 +1,24 @@
-# Video to Photos(1280x960)
 import cv2
+
 count=0
 frame=6
-video= cv2.VideoCapture("c.mp4")
+shot=0
+
+video= cv2.VideoCapture("vid.mp4")
 success, image = video.read()
+
 while success:
     success,image = video.read()
-    if frame==6:
+
+    if frame==30:
         resize = cv2.resize(image, (1280, 960)) 
         cv2.imwrite("%03d.jpg" % count, resize)
         count+=1
         frame=0
+        shot+=1
+        print("SHOT! > " + str(shot))
+        
     else:
         frame+=1
+                                
+print("Done! Photo value > " + str(shot))
