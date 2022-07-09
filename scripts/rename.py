270772a2c14b0043af221076156a5309277606a0
RussianBear391 <95121773+RussianBear391@users.noreply.github.com>
2022-07-09 11:26:42 +0300

    Update rename.py

--- scripts/rename.py
+++ scripts/rename.py
@@ -1,4 +1,4 @@
 import os
 count = 1
 for file_name in os.listdir("pic/"):
-    os.rename("pic/"+file_name, "pic/"+ "8" +str(count)+file_name)
+    os.rename("pic/"+file_name, "pic/"+ str(count)+file_name)
