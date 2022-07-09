c368bd8149f24a31a880fde48a5fc1ad3e733b82
RussianBear391 <russianbear39157@vk.com>
2022-07-09 14:36:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-# Main file
-from ui.video import RealWidget
+from PyQt5.QtWidgets import *
+import sys
+from PyQt5.QtGui import *
+from PyQt5 import *
 
-from PyQt5 import QtWidgets
 
-if __name__ == '__main__':
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    w = RealWidget()
-    w.resize(1000, 800)
-    w.showMaximized()
+class MainWindow(QMainWindow): 
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi()
+    def setupUi(self):
+        self.setWindowTitle("MainInterface") 
+
+        stacked_widget =  QStackedWidget()
+
+        layout =  QVBoxLayout()
+        layout.addWidget(stacked_widget)
+        self.setLayout(layout)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    
+    win.show()
     sys.exit(app.exec_())
