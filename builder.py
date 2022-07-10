167c2d7c93201ab866f13c719388c6120f4bb770
RussianBear391 <russianbear39157@vk.com>
2022-07-10 09:06:09 +0300

    Create builder.py

---
+++ builder.py
@@ -0,0 +1,18 @@
+from PyQt4 import QtCore, QtGui, QtDesigner
+from myui import Ui_Dialog
+
+def dump_ui(widget, path):
+    builder = QtDesigner.QFormBuilder()
+    stream = QtCore.QFile(path)
+    stream.open(QtCore.QIODevice.WriteOnly)
+    builder.save(stream, widget)
+    stream.close()
+
+app = QtGui.QApplication([''])
+
+dialog = QtGui.QDialog()
+Ui_Dialog().setupUi(dialog)
+
+dialog.show()
+
+dump_ui(dialog, 'myui.ui')
