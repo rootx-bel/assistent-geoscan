83a64706ca2f38039bbe9522c9d620df1b32b8c3
Ilya Danilenko <ilya@eadsoft.com>
2022-07-15 16:44:09 +0300

    Design

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from PyQt5.QtWidgets import *
 import sys
-import os
 from PyQt5.QtGui import *
 from PyQt5 import *
-from cv2 import resize
 from ui.video import RealWidget
 from ui.settings import Settings
-from ui.start import StartWidget
-from PyQt5.QtCore import Qt
-
-
+from ui.home import HomeWidget
 
 class MainWidget(QWidget):
     def __init__(self, parent=None):
@@ -20,84 +15,32 @@ class MainWidget(QWidget):
         self.layout = QVBoxLayout(self)
         self.setContentsMargins(0, 0, 0, 0)
 
-        self.stacked_widget = QStackedWidget()
+        self.stacked_widget = QStackedWidget(self)
         self.stacked_widget.setContentsMargins(0, 0, 0, 0)
-        self.start_buttons = StartWidget()
-        self.setts = Settings(self.set_menu)
+        self.home_widget = HomeWidget(self)
+        self.settings_widget = Settings(self.set_menu)
         self.real_w = RealWidget()
         
-        self.stacked_widget.addWidget(self.start_buttons)
+        self.stacked_widget.addWidget(self.home_widget)
         self.stacked_widget.addWidget(self.real_w)
-        self.stacked_widget.addWidget(self.setts)
+        self.stacked_widget.addWidget(self.settings_widget)
         
+        self.stacked_widget.setCurrentWidget(self.home_widget)
         
-        self.stacked_widget.setCurrentWidget(self.start_buttons)
-        
+        self.layout.setContentsMargins(0, 0, 0, 0)
         self.layout.addWidget(self.stacked_widget)
-        self.start_buttons.liveButton.clicked.connect(self.set_live)
-        self.start_buttons.loadButton.clicked.connect(self.set_menu)
-        self.start_buttons.settingsButton.clicked.connect(lambda: self.stacked_widget.setCurrentWidget(self.setts))
 
-        self.geo_label = QLabel()
-        self.geo_label.setPixmap(QPixmap("ui/images/logo.png"))
-        self.layout.addWidget(self.geo_label, alignment = Qt.AlignCenter | Qt.AlignRight )
-
-        self.layout.addStretch()
-
-        self.logosLabel = QLabel()
-        self.logosLabel.setPixmap(QPixmap("ui/images/logos.png"))
-        self.layout.addWidget(self.logosLabel, alignment= Qt.AlignRight | Qt.AlignBottom)
-
-
-        
+    def exit_click(self):
+        sender = self.sender()
+        print(sender.text)
+        # self.QCoreApplication.instance().quit
         
     def set_live(self):
         self.stacked_widget.setCurrentWidget(self.real_w)
         self.real_w.display.th.start()
         
     def set_menu(self):
-        self.stacked_widget.setCurrentWidget(self.start_buttons)
-
-
-        
-
-# class MainWidget(QWidget):
-#     def __init__(self, parent=None):
-#         super().__init__(parent)
-#         self.setupUI()
-
-#     def setupUI(self):
-#         self.layout = QVBoxLayout(self)
-
-#         self.stacked_widget = QStackedWidget()
-#         self.start_buttons = StartWidget()
-#         self.setts = Settings(self.set_menu)
-#         self.real_w = RealWidget()
-        
-#         self.stacked_widget.addWidget(self.start_buttons)
-#         self.stacked_widget.addWidget(self.real_w)
-#         self.stacked_widget.addWidget(self.setts)
-        
-        
-#         self.stacked_widget.setCurrentWidget(self.start_buttons)
-        
-#         self.layout.addWidget(self.stacked_widget)
-#         self.start_buttons.liveButton.clicked.connect(self.set_live)
-#         self.start_buttons.loadButton.clicked.connect(self.set_menu)
-#         self.start_buttons.settingsButton.clicked.connect(lambda: self.stacked_widget.setCurrentWidget(self.setts))
-
-#         self.logosLabel = QLabel(self)
-#         self.logosLabel.setPixmap(QPixmap("ui/images/logo.png"))
-#         self.layout.addWidget(self.logosLabel)
-        
-#     def set_live(self):
-#         self.stacked_widget.setCurrentWidget(self.real_w)
-#         self.real_w.display.th.start()
-        
-#     def set_menu(self):
-#         self.stacked_widget.setCurrentWidget(self.start_buttons)
-
-
+        self.stacked_widget.setCurrentWidget(self.home_widget)
 
 class MainWindow(QMainWindow): 
     def __init__(self, parent=None):
@@ -105,15 +48,8 @@ class MainWindow(QMainWindow):
         self.setupUi()
 
     def setupUi(self):
-        self.setWindowTitle("MainInterface") 
-       
-        self.setStyleSheet(
-            """
-            MainWindow{
-                border-image: url("ui/images/log.png") 0 0 0 0 stretch stretch
-            }
-            """
-        )
+        self.setContentsMargins(0, 0, 0, 0)
+        self.setWindowTitle("MainInterface")
 
         self.mainWidget = MainWidget()
         self.setCentralWidget(self.mainWidget)
@@ -121,6 +57,12 @@ class MainWindow(QMainWindow):
             
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
+    with open("ui/style.css", "r") as f:
+        _style = f.read()
+        app.setStyleSheet(_style)
+
     win = MainWindow()
+    win.resize(1920, 1080)
     win.show()
     sys.exit(app.exec_())
---
+++ ui/home.py
@@ -0,0 +1,97 @@
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QWidget, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt, QEvent, pyqtSignal, QObject
+
+class ButtonCommunicate(QObject):
+    menu_click = pyqtSignal()
+
+class Buttons(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.buttons_signal = ButtonCommunicate()
+
+        self.layout = QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 80, 0, 0)
+
+        self.load = QLabel()
+        self.load.setObjectName("load")
+        self.load.setPixmap(QPixmap("ui/images/load.png"))
+
+        self.live = QLabel()
+        self.live.setObjectName("live")
+        self.live.setPixmap(QPixmap("ui/images/live.png"))
+
+        self.settings = QLabel()
+        self.settings.setObjectName("settings")
+        self.settings.setPixmap(QPixmap("ui/images/settings.png"))
+
+        self.exit = QLabel()
+        self.exit.setObjectName("exit")
+        self.exit.setPixmap(QPixmap("ui/images/exit.png"))
+
+        self.layout.addWidget(self.load, alignment=Qt.AlignBottom)
+        self.layout.addWidget(self.live, alignment=Qt.AlignBottom)
+        self.layout.addWidget(self.settings, alignment=Qt.AlignBottom)
+        self.layout.addWidget(self.exit, alignment=Qt.AlignBottom)
+        self.load.installEventFilter(self)
+        self.live.installEventFilter(self)
+        self.settings.installEventFilter(self)
+        self.exit.installEventFilter(self)
+        self.setLayout(self.layout)
+
+    def eventFilter(self, object, event):
+        # print(object.objectName())
+        if event.type() == QEvent.Enter:
+            object.setPixmap(
+                QPixmap(f"ui/images/{object.objectName()}-hover.png"))
+            self.stop = True
+            return True
+        elif event.type() == QEvent.Leave:
+            object.setPixmap(
+                QPixmap(f"ui/images/{object.objectName()}.png"))
+            self.stop = False
+        elif event.type() == QEvent.MouseButtonPress:
+            print("Clicked")
+            self.buttons_signal.emit()
+        return False
+
+
+class ButtonsMenu(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.layout = QHBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.dots = QLabel()
+        self.dots.setObjectName("dots")
+        self.dots.setPixmap(QPixmap("ui/images/alldots.png"))
+        self.layout.addWidget(self.dots, alignment=Qt.AlignVCenter)
+
+        self.buttons = Buttons(self)
+        self.layout.addWidget(self.buttons, alignment=Qt.AlignVCenter)
+
+        self.setLayout(self.layout)
+
+
+class HomeWidget(QLabel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setObjectName('home-widget')
+
+        self.layout = QHBoxLayout()
+        self.layout.addStretch(1)
+        self.vmenu = ButtonsMenu(self)
+        self.layout.addWidget(self.vmenu)
+        self.logo = QLabel(self)
+        self.logo.setPixmap(QPixmap("ui/images/logo.png"))
+
+        self.layout.addStretch(10)
+        self.layout.addWidget(self.logo, alignment=Qt.AlignVCenter)
+
+        self.partners = QLabel(self)
+        self.partners.setPixmap(QPixmap("ui/images/partners.png"))
+        self.layout.addWidget(self.partners,
+                              alignment=Qt.AlignBottom | Qt.AlignRight, stretch=1)
+
+        self.setLayout(self.layout)
